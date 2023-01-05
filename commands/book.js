4ea6e783fb8c163d9e7432f29090638e4d816a3f
Quirin Brauer <quirin.brauer@web.de>
2023-01-05 18:54:05 +0100

    /book done

--- commands/book.js
+++ commands/book.js
@@ -9,16 +9,33 @@ module.exports = {
   name: "book",
   description: "this is a ping command!",
   async execute(client, interaction) {
-    // console.log(await bookingsdb.all())
+   
     var flightnumber = interaction.options.getString("flight");
+    var bookingdball=await bookingsdb.all()
+    console.log(bookingdball)
     if (!(await flightdb.has(flightnumber))) {
       interaction.reply({
         embeds: [respond("An error encourred!", "flight not found!")],
       });
       return;
     }
-    var flight = await flightdb.get(flightnumber);
-    console.log(flight);
+   for(let i=0;i<bookingdball.length;i++)
+   {
+    if(bookingdball[i].value.user==interaction.user.id)
+    {
+      interaction.reply({ embeds: [respond("An error encourred!", "You already booked this flight!")],});
+      return
+    }
+   }
+   var flight = await flightdb.get(flightnumber);
+   if(flight.bookedseats>=flight.maxpassengers)
+   {
+    interaction.reply({ embeds: [respond("An error encourred!", "All seats have already been booked!")],});
+      return
+   }
+   
+    
+   
     const bookedclass = interaction.options.getString("class");
     var bookingnumber = (Math.random() * (9999 - 1) + 1) .toString() .split(".")[0];
     if ((await bookingsdb.all().length) != undefined) {
@@ -30,10 +47,11 @@ module.exports = {
       }
      
     }
-    console.log(bookingnumber);
+   // console.log(bookingnumber);
     await bookingsdb.set(bookingnumber,{user:interaction.user.id,flightnumber:flightnumber,class:bookedclass})
+    await flightdb.add(`${flightnumber}.bookedseats`,1)
     interaction.reply({
-        embeds: [respond("Successfully booked flight!", "Your bookingnumber is: \n"+bookingnumber)],
+        embeds: [respond("Successfully booked flight!", "Your bookingnumber is: \n**"+bookingnumber+"**")],
       });
   }
 };
